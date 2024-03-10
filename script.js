1dbd114eeba1b21cb29d7c6c66af3daa83a8655d
Jason Nguyen <j1.nguyen@yahoo.com>
2024-03-10 00:40:27 -0800

    completed tictactoe

--- script.js
+++ script.js
@@ -55,8 +55,54 @@ function handleClick(evt) {
     rowIdx = parseInt(boxId.charAt(3));
     if(board[colIdx][rowIdx]) return;
     board[colIdx][rowIdx] = turn;
+    winner = checkWinner(colIdx, rowIdx);
     turn *= -1;
     render();
+};
+
+function checkWinner(colIdx, rowIdx) {
+    return checkVerticalWin(colIdx, rowIdx) || checkHorizontalWin(colIdx, rowIdx) || checkNeSwWin(colIdx, rowIdx) || checkNwSeWin(colIdx, rowIdx) || checkTie();
+};
+
+function checkTie() {
+    if(!board[0].includes(null) && !board[1].includes(null) && !board[2].includes(null)) return "T";
+}
+
+function checkNwSeWin(colIdx, rowIdx) {
+    const adjCount = checkAdjacent(colIdx, rowIdx, 1, -1) + checkAdjacent(colIdx, rowIdx, -1, 1);
+    return adjCount === 2 ? board[colIdx][rowIdx] : null;
+};
+
+function checkNeSwWin(colIdx, rowIdx) {
+
+    const adjCount = checkAdjacent(colIdx, rowIdx, 1, 1) + checkAdjacent(colIdx, rowIdx, -1, -1);
+    return adjCount === 2 ? board[colIdx][rowIdx] : null;
+
+};
+
+function checkHorizontalWin(colIdx, rowIdx) {
+    const adjCount = checkAdjacent(colIdx, rowIdx, 1, 0) + checkAdjacent(colIdx, rowIdx, -1, 0);
+    return adjCount === 2 ? board[colIdx][rowIdx] : null;
+};
+
+function checkVerticalWin(colIdx, rowIdx) {
+    const adjCount = checkAdjacent(colIdx, rowIdx, 0, -1) + checkAdjacent(colIdx, rowIdx, 0, 1)
+    return adjCount === 2 ? board[colIdx][rowIdx] : null;
+};
+
+function checkAdjacent(colIdx, rowIdx, colOffset, rowOffset) {
+    let count = 0;
+    const playerValue = board[colIdx][rowIdx];
+
+    // perform the offset to begin checking adjacent cells
+    colIdx += colOffset;
+    rowIdx += rowOffset;
+    while (board[colIdx] && board[colIdx][rowIdx] === playerValue) {
+        count++;
+        colIdx += colOffset;
+        rowIdx += rowOffset;
+    };
+    return count;
 };
 
 function render() {
@@ -71,14 +117,15 @@ function renderControls() {
         colArray.forEach(function(cellValue, rowIdx){
             const cellId = `c${colIdx}r${rowIdx}`;
             const cellEl = document.getElementById(cellId);
-            console.log(cellValue)
-            const boxVis = !cellEl.innerText
-            if(cellValue) {
-                cellEl.style.border = "0";
+            if(winner) {
+                cellEl.style.border = "hidden";
+                document.getElementById("board").removeEventListener("click", handleClick);
+            } else if(cellValue) {
+                cellEl.style.border = "hidden";
                 cellEl.style.transform = "none";
+            } else {
+                cellEl.removeAttribute("style");
             }
-            // cellEl.style.border = boxVis ? "0.1vmin solid grey" : "0";
-            // cellEl.style.transform = boxVis ? "" : "none"
         });
         
     });
